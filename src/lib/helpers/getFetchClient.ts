c382c17e198ad08092911129a3ab0abff6cf700e
Shiraz Khan <shirazahmad200@gmail.com>
2026-07-28 20:31:09 +0500

    Update getFetchClient.ts

--- src/lib/helpers/getFetchClient.ts
+++ src/lib/helpers/getFetchClient.ts
@@ -54,8 +54,8 @@ function getStickyProxyAddress(
     const proxyUrl = new URL(proxyAddress);
     if (!proxyUrl.username) return proxyAddress;
 
-    //proxyUrl.username = `${proxySessionId}__${proxyUrl.username}`;
-    proxyUrl.username = `${proxyUrl.username}`;
+    proxyUrl.username = `${proxySessionId}__${proxyUrl.username}`;
+    //proxyUrl.username = `${proxyUrl.username}`;
     return proxyUrl.toString();
 }
 
