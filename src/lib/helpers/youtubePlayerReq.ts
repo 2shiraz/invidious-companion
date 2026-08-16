c39321f836f2b3ca8fd0b64672bae2c0dd3d213b
Shiraz Khan <shirazahmad200@gmail.com>
2026-08-16 22:06:25 +0500

    caching: longer caching

--- src/lib/helpers/youtubePlayerReq.ts
+++ src/lib/helpers/youtubePlayerReq.ts
@@ -18,7 +18,7 @@ import { getFetchClient } from "./getFetchClient.ts";
 
 const enableCompression = true;
 const TV_CONFIG_CACHE_TTL_MS = 15 * 60 * 1000;
-const REDIRECTOR_CACHE_TTL_MS = 5 * 1 * 1000;
+const REDIRECTOR_CACHE_TTL_MS = 15 * 60 * 1000;
 
 type CacheEntry<T> = {
     expiresAt: number;
